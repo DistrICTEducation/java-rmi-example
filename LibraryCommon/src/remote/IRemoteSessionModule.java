62e13ade584d3fb817248256b9592d73c66a23c1
JorisSchelfaut <joris.schelfaut@hotmail.com>
2014-03-14 16:34:40 +0100

    LibraryRMI : common library.

--- LibraryCommon/src/remote/IRemoteSessionModule.java
+++ LibraryCommon/src/remote/IRemoteSessionModule.java
@@ -3,6 +3,7 @@ package remote;
 import exceptions.AuthenticationException;
 import args.Session;
 import java.rmi.Remote;
+import java.rmi.RemoteException;
 
 /**
  * IRemoteSessionModule manages user sessions. The interface provides
@@ -18,18 +19,21 @@ public interface IRemoteSessionModule extends Remote {
      * @param password the password of the user to authenticate.
      * @return the session for the authenticated user.
      * @throws AuthenticationException
+     * @throws RemoteException
      */
     public Session authenticate(String username, String password)
-            throws AuthenticationException;
+            throws AuthenticationException, RemoteException;
 
     /**
      * @param session the session to validate.
      * @return whether or not the session is valid.
+     * @throws RemoteException
      */
-    public boolean isAuthenticated(Session session);
+    public boolean isAuthenticated(Session session) throws RemoteException;
     
     /**
      * @param username the username of the user to log off.
+     * @throws RemoteException
      */
-    public void destroySession(String username);
+    public void destroySession(String username) throws RemoteException;
 }
